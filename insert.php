408e0a13c8b45ba85a4e8e7110bfb9c1961b36c5
abdupky <56450735+abdupky@users.noreply.github.com>
2019-10-13 21:34:47 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,24 +2,50 @@
 
 $name=$_POST['name'];
 
-$occup=$_POST['occup'];
-$gender=$_POST['gender'];
 $dob=$_POST['dob'];
 
 $email=$_POST['email'];
+$password=$_POST['password'];
 $mob=$_POST['mob'];
-$branch=$_POST['branch'];
-$semester=$_POST['semester'];
-$address=$_POST['address'];
 
-$link = mysqli_connect("localhost", "root", "", "gec");
+$temp=$name.date("d_m_y");
+if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $expensions= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$expensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true){
+		// $temp=$name.date("d.m.y"); 
+	   //rename($file_name,$name);
+         move_uploaded_file($file_tmp,"image/".$temp);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+
+
+$link = mysqli_connect("localhost", "id7445712__root", "jithu", "id7445712__book");
 
 if($link === false){
 
     die("ERROR: Could not connect. " . mysqli_connect_error());
 
 }
-$sql = "INSERT INTO gec(name,occup,gender,dob,email,mob,branch,semester,address) VALUES ('$name','$occup','$gender','$dob','$email','$mob','$branch','$semester','$address')";
+$sql = "INSERT INTO gec(name,dob,email,password,mob,image) VALUES ('$name','$dob','$email','$password','$mob','$temp')";
 
 if(mysqli_query($link, $sql)){
 
@@ -33,3 +59,4 @@ if(mysqli_query($link, $sql)){
 mysqli_close($link);
 
 ?>
+
